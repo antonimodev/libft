071d2a5a86b867a461115ab57a00f2c463e4e07c
Antonio Luis Moreno Del Pino <antonimo@c3r2s3.42malaga.com>
2024-04-09 17:53:45 +0200

    Strlcpy, main hecho, tener en cuenta para comprobar con la funcion original

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,36 @@
 /*   By: antonimo <antonimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:48:27 by antonimo          #+#    #+#             */
-/*   Updated: 2024/04/09 16:47:46 by antonimo         ###   ########.fr       */
+/*   Updated: 2024/04/09 17:52:02 by antonimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
+#include <stdio.h>
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int	i;
+	size_t	srclenght;
+	int		i;
+
+	srclenght = ft_strlen(src);
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (srclenght);
 }
+
+/* int	main(void)
+{
+	char	str[20];
+	char	str2[20];
+
+	printf("Resultado de mi funcion; %lu.\n", ft_strlcpy(str,"hola", 3));
+	printf("Resultado de mi funcion; %lu.\n", strlcpy(str2,"hola", 3));
+	printf("Mi función es: %s.\n", str);
+	printf("Mi función es: %s.\n", str2);
+} */
--- libft.h
+++ libft.h
@@ -6,26 +6,34 @@
 /*   By: antonimo <antonimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:22:19 by antonimo          #+#    #+#             */
-/*   Updated: 2024/04/09 15:57:31 by antonimo         ###   ########.fr       */
+/*   Updated: 2024/04/09 17:52:40 by antonimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
+# include <string.h>
 /**
  * @brief Identify if the given value is a digit
  * 
  * @param c 
  * @return int 
  */
-int	ft_isdigit(int c);
+int		ft_isdigit(int c);
 /**
  * @brief Identify if the given value is an alphabetic character
  * 
  * @param c 
  * @return int 
  */
-int	ft_isalpha(int c);
+int		ft_isalpha(int c);
+
+/**
+ * @brief String lenght
+ * 
+ * @param s 
+ * @return size_t 
+ */
+size_t	ft_strlen(const char *s);
 
 #endif
